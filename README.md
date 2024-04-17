6b1b3ee4eefb9ea24cec43cbfd486c905739791e
Martin Haas <martinhaas2005@gmail.com>
2024-04-16 21:19:38 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dastral
+
+***include readme later***
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! # Dastral
+//! A library for learning and reducing boilerplate code related to
+//! data structures and algorithms.
+//!
+//! Dastral is a blend of the words `(Da)ta`, `(Str)ucture` and `(Al)gorithm`,
+//! forming a unique and memorable name.
